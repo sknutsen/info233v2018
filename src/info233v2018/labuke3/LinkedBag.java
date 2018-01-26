3da2f4fc8046526b7a6e8a4909912b10de5c6712
sknutsen <sondreknutsen1@gmail.com>
2018-01-26 08:31:39 +0100

    commit

--- src/info233v2018/labuke3/LinkedBag.java
+++ src/info233v2018/labuke3/LinkedBag.java
@@ -8,6 +8,7 @@ public final class LinkedBag<T> implements BagInterface<T> {
     public LinkedBag(){
         firstNode = null;
         numberOfEntries = 0;
+        initialized = true;
     }
 
     @Override
@@ -17,27 +18,70 @@ public final class LinkedBag<T> implements BagInterface<T> {
 
     @Override
     public boolean isEmpty() {
-        return false;
+        boolean result = false;
+        if (getInitialized() == false){
+            result = true;
+        } else if (firstNode == null){
+            result = true;
+        }
+        return result;
     }
 
     @Override
-    public boolean add(Object newEntry) {
-        return false;
+    public boolean add(T newEntry) {
+        Node newNode = new Node(newEntry);
+        firstNode = newNode;
+        newNode.next = firstNode;
+        numberOfEntries++;
+
+        return true;
     }
 
     @Override
     public T remove() {
-        return null;
+        T result = null;
+        if (!isEmpty()) {
+            if (firstNode != null) {
+                result = firstNode.data;
+                firstNode = firstNode.next;
+                numberOfEntries--;
+            }
+        }
+        return result;
+    }
+
+    private Node getReferenceTo(T entry){
+        boolean found = false;
+        Node currentNode = firstNode;
+
+        while (!found && (currentNode != null)){
+            if (entry.equals(currentNode.data)){
+                found = true;
+            } else {
+                currentNode = currentNode.next;
+            }
+        }
+        return currentNode;
     }
 
     @Override
-    public boolean remove(Object o) {
-        return false;
+    public boolean remove(T o) {
+        boolean result = false;
+        Node nodeN = getReferenceTo(o);
+        if (nodeN != null) {
+            nodeN.data = firstNode.data;
+            firstNode = firstNode.next;
+            numberOfEntries--;
+            result = true;
+        }
+        return result;
     }
 
     @Override
     public void clear() {
-
+        while(!isEmpty()){
+            remove();
+        }
     }
 
     @Override
@@ -63,6 +107,28 @@ public final class LinkedBag<T> implements BagInterface<T> {
         this.initialized = init;
     }
 
+    public T replace(T t){
+        getInitialized();
+        T result = remove();
+        add(t);
+        return result;
+    }
+
+    public T getMin(){
+        Node result = null;
+        if (firstNode.next != null){
+
+        } else {
+            result = firstNode;
+        }
+        return result.data;
+    }
+
+    public T getMin(Node node){
+        Node result = null;
+        return result.data;
+    }
+
     private class Node {
         private T data;
         private Node next;
@@ -76,5 +142,11 @@ public final class LinkedBag<T> implements BagInterface<T> {
             next = nextNode;
         }
     }
-
+    /**
+     * exercise 3.2 a
+     *  returns the contents of the bag as a line of text in the console
+     *
+     *  exercise 3.2 b
+     *
+     */
 }
---
+++ src/info233v2018/labuke4/StackInterface.java
@@ -0,0 +1,67 @@
+package info233v2018.labuke4;
+
+public interface StackInterface {
+}
+/**
+ * 5.1
+ * it will remove the object that was added last first
+ *
+ * 5.2
+ *
+ *
+ * 5.3
+ * nothing
+ *
+ * 5.4
+ * 111
+ * 222
+ * 333
+ *
+ * 5.5.a
+ * 6
+ *
+ * 5.5.b
+ * nth triangle number
+ *
+ * 5.6
+ * a { {[ {[( {[ { null
+ * b { {( { {[ { {) )
+ * c {])
+ *
+ * 5.7
+ * a ab*cd-/
+ * b abc * - de * f * g + /
+ * c ab / c de - + *
+ * d a^b cd - *
+ *
+ * 5.8
+ * a 15
+ * b 33
+ * c 241
+ *
+ * 5.9
+ * a + ab * c - d
+ * b + / * ab - ca * de
+ * c - ac + b^d
+ *
+ * 5.10
+ *
+ *
+ * 6.1
+ * idk
+ *
+ * 6.2
+ * a.
+ * b. no because then you would only have access to the item that was added last
+ *
+ * 6.5
+ * remove(n)
+ * while n > 0 && !isEmpty()
+ * pop()
+ * n--
+ *
+ * 6.6
+ *
+ *
+ * 6.7
+ */
